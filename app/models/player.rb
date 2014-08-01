6f4fbb2bc40cbbb41293fd3967bf070b6389d935
Geoff Schorkopf <gschorkopf@gmail.com>
2014-08-01 17:18:42 -0400

    Adds three rank methods to player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,19 @@ class Player < ActiveRecord::Base
   serialize :day_ranks
   validates :nerd_id, uniqueness: true
   before_create :set_todays_day_rank_empty
+
+  def overall_rank(date = Date.today)
+    day_ranks[date][:overall_rank]
+  end
+
+  def nerd_rank(date = Date.today)
+    day_ranks[date][:nerd_rank]
+  end
+
+  def position_rank(date = Date.today)
+    day_ranks[date][:position_rank]
+  end
+
   private
 
   def set_todays_day_rank_empty
