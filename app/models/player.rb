2c113a6aed78da8ecada43d8a91bea0178a426ff
Geoff Schorkopf <gschorkopf@gmail.com>
2014-08-01 17:17:49 -0400

    Serializes day_ranks in Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,10 @@
 class Player < ActiveRecord::Base
+  serialize :day_ranks
   validates :nerd_id, uniqueness: true
+  before_create :set_todays_day_rank_empty
+  private
+
+  def set_todays_day_rank_empty
+    self.day_ranks ||= {Date.today => {}}
+  end
 end
