c4ae6b38176bb15e8a8b40ece72d1261173ab352
Geoff Schorkopf <gschorkopf@gmail.com>
2014-08-05 17:27:14 -0400

    Adds basic stats to dashboard#show

--- app/decorators/player_decorator.rb
+++ app/decorators/player_decorator.rb
@@ -1,5 +1,19 @@
 class PlayerDecorator < Draper::Decorator
   delegate_all
 
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 
+  def display_overall_rank
+    "##{overall_rank}"
+  end
+
+  def display_position_rank
+    "##{position_rank} amongst #{position}s"
+  end
+
+  def display_nerd_rank
+    "Aggregate Rank: #{nerd_rank}"
+  end
 end
